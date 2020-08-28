5d076dc591b37acea3e8da366031d0330d2713f5
Summer.zhou <skyjimzhoujie@gmail.com>
2020-08-28 15:56:22 +0800

    新增ZJHiddenApiUtilsDemo

--- app/src/main/java/com/zj/tools/devutils/MainActivity.java
+++ app/src/main/java/com/zj/tools/devutils/MainActivity.java
@@ -1,14 +1,111 @@
 package com.zj.tools.devutils;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    public static final String ITEM_1 = "1. 访问@hide注释的函数";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.list);
+
+        final ArrayList<String> data = new ArrayList<>();
+        data.add(ITEM_1);
+        final MainAdapter adapter = new MainAdapter(data);
+        adapter.setListener(new OnAdapterListener() {
+            @Override
+            public void onItemClick(String title) {
+                switch (title) {
+                    case ITEM_1:
+                        ZJHiddenApiUtilsDemoActivity.launch(MainActivity.this);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(adapter);
+    }
+
+    public interface OnAdapterListener {
+        void onItemClick(String title);
+    }
+
+    private static class MainAdapter extends RecyclerView.Adapter {
+
+        private final List<String> data;
+        private OnAdapterListener listener;
+
+        MainAdapter(List<String> data) {
+            this.data = data;
+        }
+
+        public void setListener(OnAdapterListener listener) {
+            this.listener = listener;
+        }
+
+        private View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    final String title = (String) v.getTag();
+                    listener.onItemClick(title);
+                }
+            }
+        };
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            final View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_adapter_item, parent, false);
+            return new MyViewHodler(inflate);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            MyViewHodler h = (MyViewHodler) holder;
+            String title = data.get(position);
+            h.title.setText(title);
+            h.itemView.setTag(title);
+            h.itemView.setOnClickListener(clickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return this.data == null ? 0 : data.size();
+        }
+
+        private class MyViewHodler extends RecyclerView.ViewHolder {
+
+            TextView title;
+
+            public MyViewHodler(View inflate) {
+                super(inflate);
+                itemView.setBackground(itemView.getResources().getDrawable(R.drawable.list_item_bg));
+                title = inflate.findViewById(R.id.tv_value);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/zj/tools/devutils/ZJHiddenApiUtilsDemoActivity.java
@@ -0,0 +1,93 @@
+package com.zj.tools.devutils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zj.tools.mylibrary.ZJHiddenApiUtils;
+
+import java.lang.reflect.Method;
+
+public class ZJHiddenApiUtilsDemoActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // variables
+
+    private Button directInvoke;
+    private Button invokeByUtils;
+    private TextView tvContent;
+    final long SIZE_1_5M = 1024 * 1024 * 1 + 1024 * 500;//1.5M
+    private static final String TAG = "ZJHiddenApiUtilsDemoActivity";
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // func-out
+
+    public static void launch(Context context) {
+        context.startActivity(new Intent(context, ZJHiddenApiUtilsDemoActivity.class));
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // in-func
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_z_j_hidden_api_utils_demo);
+        initView();
+    }
+
+    private void initView() {
+        directInvoke = findViewById(R.id.direct_invoke);
+        directInvoke.setOnClickListener(this);
+        invokeByUtils = findViewById(R.id.invoke_by_utils);
+        invokeByUtils.setOnClickListener(this);
+        tvContent = findViewById(R.id.tv_content);
+        tvContent.setMovementMethod(ScrollingMovementMethod.getInstance());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.direct_invoke:
+                invokeHideMethod();
+                break;
+            case R.id.invoke_by_utils:
+//                ZJHiddenApiUtils.exemptAll();
+                ZJHiddenApiUtils.setExemption("Landroid/util/DebugUtils;->sizeValueToString(JLjava/lang/StringBuilder;)Ljava/lang/String;");//;
+                invokeHideMethod();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void invokeHideMethod() {
+        try {
+            final Class<?> class_DebugUtils = Class.forName("android.util.DebugUtils");
+            final Method m_SizeValueToString = class_DebugUtils.getDeclaredMethod("sizeValueToString", long.class, StringBuilder.class);
+            final String result = (String) m_SizeValueToString.invoke(null, SIZE_1_5M, null);
+            printResult(result);
+        } catch (Exception e) {
+            printResult(Log.getStackTraceString(e));
+        }
+    }
+
+    private void printResult(String result) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("调用 android.util.DebugUtils.sizeValueToString(long, StringBuilder)");
+        builder.append("求得传入的 bytes = 1024 * 1024 * 1 + 1024 * 500 (≈1.5M) 合适的大小表示字符串");
+        builder.append("\n\n");
+        builder.append(result);
+        tvContent.setText(builder.toString());
+    }
+}
