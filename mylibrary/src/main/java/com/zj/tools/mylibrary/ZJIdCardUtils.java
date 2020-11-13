08dbd57cb87ea2f4f2791cf53f77deaae43d1f1c
Summer.zhou <skyjimzhoujie@gmail.com>
2020-11-13 14:12:24 +0800

    本地校验身份证格式

---
+++ mylibrary/src/main/java/com/zj/tools/mylibrary/ZJIdCardUtils.java
@@ -0,0 +1,173 @@
+package com.zj.tools.mylibrary;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 身份证工具类
+ * <p>
+ * github:https://github.com/summer-zhoujie/ZJUtils
+ */
+public class ZJIdCardUtils {
+
+    /**
+     * 是否是格式正确的身份证 ( 本地校验 )
+     *
+     * @param IDNumber 输入身份证号码
+     * @return true 格式正确
+     */
+    public static boolean isIdCardValid(String IDNumber) {
+        if (IDNumber == null || "".equals(IDNumber)) {
+            return false;
+        }
+        // 定义判别用户身份证号的正则表达式（15位或者18位，最后一位可以为字母）
+        String regularExpression = "(^[1-9]\\d{5}(18|19|20)\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|" +
+                "(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}$)";
+        //假设18位身份证号码:41000119910101123X  410001 19910101 123X
+        //^开头
+        //[1-9] 第一位1-9中的一个      4
+        //\\d{5} 五位数字           10001（前六位省市县地区）
+        //(18|19|20)                19（现阶段可能取值范围18xx-20xx年）
+        //\\d{2}                    91（年份）
+        //((0[1-9])|(10|11|12))     01（月份）
+        //(([0-2][1-9])|10|20|30|31)01（日期）
+        //\\d{3} 三位数字            123（第十七位奇数代表男，偶数代表女）
+        //[0-9Xx] 0123456789Xx其中的一个 X（第十八位为校验值）
+        //$结尾
+
+        //假设15位身份证号码:410001910101123  410001 910101 123
+        //^开头
+        //[1-9] 第一位1-9中的一个      4
+        //\\d{5} 五位数字           10001（前六位省市县地区）
+        //\\d{2}                    91（年份）
+        //((0[1-9])|(10|11|12))     01（月份）
+        //(([0-2][1-9])|10|20|30|31)01（日期）
+        //\\d{3} 三位数字            123（第十五位奇数代表男，偶数代表女），15位身份证不含X
+        //$结尾
+
+
+        boolean matches = IDNumber.matches(regularExpression);
+
+        //判断第18位校验值
+        if (matches) {
+
+            if (IDNumber.length() == 18) {
+                try {
+                    char[] charArray = IDNumber.toCharArray();
+                    //前十七位加权因子
+                    int[] idCardWi = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+                    //这是除以11后，可能产生的11位余数对应的验证码
+                    String[] idCardY = {"1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2"};
+                    int sum = 0;
+                    for (int i = 0; i < idCardWi.length; i++) {
+                        int current = Integer.parseInt(String.valueOf(charArray[i]));
+                        int count = current * idCardWi[i];
+                        sum += count;
+                    }
+                    char idCardLast = charArray[17];
+                    int idCardMod = sum % 11;
+                    if (idCardY[idCardMod].toUpperCase().equals(String.valueOf(idCardLast).toUpperCase())) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+        }
+        return matches;
+    }
+
+    /**
+     * 判断用户的性别
+     *
+     * @param idCard 身份证号码
+     * @return -1: 失败, 1:男性, 2:女性
+     */
+    public static int getGender(String idCard) {
+        if (!isIdCardValid(idCard)) {
+            return -1;
+        }
+
+        if (idCard.length() == 15) {
+            final String substring = idCard.substring(13, 14);
+            final int i = Integer.parseInt(substring);
+            return i % 2 == 0 ? 2 : 1;
+        } else if (idCard.length() == 18) {
+            final String substring = idCard.substring(16, 17);
+            final int i = Integer.parseInt(substring);
+            return i % 2 == 0 ? 2 : 1;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * 根据身份证号码计算年龄
+     *
+     * @param idNumber 考虑到了15位身份证，但不一定存在
+     * @return 默认返回-1代表计算失败
+     */
+    public static int getAge(String idNumber) {
+        if (!isIdCardValid(idNumber)) {
+            return -1;
+        }
+
+        String dateStr;
+        if (idNumber.length() == 15) {
+            dateStr = "19" + idNumber.substring(6, 12);
+        } else if (idNumber.length() == 18) {
+            dateStr = idNumber.substring(6, 14);
+        } else {
+            return -1;
+        }
+
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        try {
+            Date birthday = simpleDateFormat.parse(dateStr);
+            return getAgeByDate(birthday);
+        } catch (ParseException e) {
+            return -1;
+        }
+
+
+    }
+
+    private static int getAgeByDate(Date birthday) {
+        Calendar calendar = Calendar.getInstance();
+
+        //calendar.before()有的点bug
+        if (calendar.getTimeInMillis() - birthday.getTime() < 0L) {
+            return -1;
+        }
+
+
+        int yearNow = calendar.get(Calendar.YEAR);
+        int monthNow = calendar.get(Calendar.MONTH);
+        int dayOfMonthNow = calendar.get(Calendar.DAY_OF_MONTH);
+
+        calendar.setTime(birthday);
+
+
+        int yearBirthday = calendar.get(Calendar.YEAR);
+        int monthBirthday = calendar.get(Calendar.MONTH);
+        int dayOfMonthBirthday = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int age = yearNow - yearBirthday;
+
+
+        if (monthNow <= monthBirthday && monthNow == monthBirthday && dayOfMonthNow < dayOfMonthBirthday || monthNow < monthBirthday) {
+            age--;
+        }
+
+        return age;
+    }
+
+}
